31e4bdca3946f0102a0abd03918bb6138c1bf00c
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-14 01:43:54 +0300

    MissionPassed

--- Lab.py
+++ Lab.py
@@ -8,5 +8,12 @@ def check_format(expression):
         if char in valid_pairs.keys():
             stack.append(char)
         elif char in valid_pairs.values():
-            if not stack in valid_pairs[stack.pop()] != char:
+            if not stack or valid_pairs[stack.pop()] != char:
                 return False
+    
+    return len(stack) == 0
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(check_format(expression1))  
+print(check_format(expression2))  
