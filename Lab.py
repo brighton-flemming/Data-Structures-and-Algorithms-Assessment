74affed870f7c954733c8bfe1aa7bfc147d90282
brighton.flemming <brighton.flemming@student.moringaschool.com>
2023-09-14 11:32:07 +0300

    Mission Passed

--- Lab.py
+++ Lab.py
@@ -17,3 +17,69 @@ expression1 = "([]{})"
 expression2 = "([)]"
 print(check_format(expression1))  
 print(check_format(expression2))  
+
+def remove_duplicates(sequence):
+
+    spotted = set()
+    result = []
+
+    for item in sequence:
+        if item not in spotted:
+            spotted.add(item)
+            result.append(item)
+
+    return result
+
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = remove_duplicates(input_sequence)
+print(result)  # Output: [2, 3, 4, 5, 6, 7]                                       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
